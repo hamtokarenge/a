8e9779801aed1d0c36d2a05688c217e8364b8ae8
hamtokarenge <aminuled.ge.50@gmail.com>
2024-11-12 05:31:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/new/hydra.py",
+    "postStartCommand": "python3 /workspaces/a/hydra.py",
     "customizations": {
         "vscode": {
             "settings": {
